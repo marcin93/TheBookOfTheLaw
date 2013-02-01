c968b7a0535c4b160c1c2a9272d0a6dae57a0c06
Marcin <marcin93@gmail.com>
2013-02-01 18:32:57 +0100

    ui change

--- src/world/thelema/bookofthelaw/Comment.java
+++ src/world/thelema/bookofthelaw/Comment.java
@@ -2,15 +2,76 @@ package world.thelema.bookofthelaw;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.util.Log;
+import android.view.GestureDetector;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 
 public class Comment extends Activity {
 
+	private GestureDetector gestureDetector;
+
+	@SuppressWarnings("deprecation")
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_comment);
+
+		gestureDetector = new GestureDetector(new SwipeGestureDetector());
+	}
+
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		if (gestureDetector.onTouchEvent(event)) {
+			return true;
+		}
+		return super.onTouchEvent(event);
+	}
+
+	private void onLeftSwipe() {
+		setContentView(R.layout.chapter_one);
+	}
+
+	private void onRightSwipe() {
+		setContentView(R.layout.credits);
+	}
+
+	// Private class for gestures
+	private class SwipeGestureDetector extends
+			GestureDetector.SimpleOnGestureListener {
+		// Swipe properties, you can change it to make the swipe
+		// longer or shorter and speed
+		private static final int SWIPE_MIN_DISTANCE = 120;
+		private static final int SWIPE_MAX_OFF_PATH = 200;
+		private static final int SWIPE_THRESHOLD_VELOCITY = 200;
+
+		@Override
+		public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX,
+				float velocityY) {
+			try {
+				float diffAbs = Math.abs(e1.getY() - e2.getY());
+				float diff = e1.getX() - e2.getX();
+
+				if (diffAbs > SWIPE_MAX_OFF_PATH)
+					return false;
+
+				// Left swipe
+				if (diff > SWIPE_MIN_DISTANCE
+						&& Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
+					Comment.this.onLeftSwipe();
+
+					// Right swipe
+				} else if (-diff > SWIPE_MIN_DISTANCE
+						&& Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
+					Comment.this.onRightSwipe();
+				}
+			} catch (Exception e) {
+				Log.e("Comment", "Error on gestures");
+			}
+			return false;
+		}
 	}
 
 	@Override
@@ -19,14 +80,18 @@ public class Comment extends Activity {
 		getMenuInflater().inflate(R.menu.menu, menu);
 		return true;
 	}
-		@Override
+
+	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		switch (item.getItemId()) {
 		case R.id.mCredits:
 			setContentView(R.layout.credits);
 			break;
 		case R.id.mExit:
-			onDestroy();
+			Intent intent = new Intent(Intent.ACTION_MAIN);
+			intent.addCategory(Intent.CATEGORY_HOME);
+			intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+			startActivity(intent);
 			break;
 		case R.id.mComment:
 			setContentView(R.layout.activity_comment);
--- src/world/thelema/bookofthelaw/Credits.java
+++ src/world/thelema/bookofthelaw/Credits.java
@@ -1,17 +1,80 @@
 package world.thelema.bookofthelaw;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.GestureDetector;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.GestureDetector.OnGestureListener;
+import android.view.GestureDetector.SimpleOnGestureListener;
 
 public class Credits extends Activity {
 
+	private GestureDetector gestureDetector;
+
+	@SuppressWarnings("deprecation")
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.credits);
+
+		gestureDetector = new GestureDetector(new SwipeGestureDetector());
+	}
+
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		if (gestureDetector.onTouchEvent(event)) {
+			return true;
+		}
+		return super.onTouchEvent(event);
+	}
+
+	private void onLeftSwipe() {
+		setContentView(R.layout.activity_comment);
+	}
+
+	private void onRightSwipe() {
+		setContentView(R.layout.chapter_three);
+	}
+
+	// Private class for gestures
+	private class SwipeGestureDetector extends SimpleOnGestureListener
+			implements OnGestureListener {
+		// Swipe properties, you can change it to make the swipe
+		// longer or shorter and speed
+		private static final int SWIPE_MIN_DISTANCE = 120;
+		private static final int SWIPE_MAX_OFF_PATH = 200;
+		private static final int SWIPE_THRESHOLD_VELOCITY = 200;
+
+		@Override
+		public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX,
+				float velocityY) {
+			try {
+				float diffAbs = Math.abs(e1.getY() - e2.getY());
+				float diff = e1.getX() - e2.getX();
+
+				if (diffAbs > SWIPE_MAX_OFF_PATH)
+					return false;
+
+				// Left swipe
+				if (diff > SWIPE_MIN_DISTANCE
+						&& Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
+					Credits.this.onLeftSwipe();
+
+					// Right swipe
+				} else if (-diff > SWIPE_MIN_DISTANCE
+						&& Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
+					Credits.this.onRightSwipe();
+				}
+			} catch (Exception e) {
+				Log.e("Credits", "Error on gestures");
+			}
+			return false;
+		}
 	}
 
 	@Override
@@ -28,7 +91,10 @@ public class Credits extends Activity {
 			setContentView(R.layout.credits);
 			break;
 		case R.id.mExit:
-			onDestroy();
+			Intent intent = new Intent(Intent.ACTION_MAIN);
+			intent.addCategory(Intent.CATEGORY_HOME);
+			intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+			startActivity(intent);
 			break;
 		case R.id.mComment:
 			setContentView(R.layout.activity_comment);
